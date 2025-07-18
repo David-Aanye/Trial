3708f0935f84a7716433e8456b4a9bcaeaa5443e
David-Aanye <dmaanye@gmail.com>
2025-07-18 13:52:35 +0000

    Third File

---
+++ question3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Author: David 
+#Date Created: June 10, 2025
+#Date Modified: June 30, 2025
+#Subject: SCRIPT TO PROMPT CREDIT WHILE USING ATM CARD
+
+credit_limit=2000
+#Welcome message
+echo "Welcome to RBC, please insert your card to proceed"
+sleep 4s
+
+#Prompt user about credit limit
